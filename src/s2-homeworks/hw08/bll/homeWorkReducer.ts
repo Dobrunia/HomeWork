4aaad8c9341308aafdb13517251bd2247db6f764
Dobrunia <dobkos@hotmail.com>
2023-06-12 22:43:56 +0300

    10

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,21 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => {
+  switch (action.type) {
+    case "sort": {
+      // by name
+      return action.payload === "up"
+        ? [...state].sort((a, b) => a.name.localeCompare(b.name))
+        : [...state].sort((a, b) => b.name.localeCompare(a.name)); 
     }
-}
+    case "check": {
+        return state.filter((user: UserType) => user.age > action.payload);
+    }
+    default:
+      return state;
+  }
+};
